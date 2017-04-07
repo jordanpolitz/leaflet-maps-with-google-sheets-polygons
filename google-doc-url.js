a9f0b29f90b54268a8e4004e2e444f234708667b
jordanpolitz <jordanpolitz@gmail.com>
2017-04-07 10:33:57 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1fnerEw6KJEh0KzxjslB9sUTcxLFDtiaqFHNpIYuye28/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1fnerEw6KJEh0KzxjslB9sUTcxLFDtiaqFHNpIYuye28/edit#gid=1716207954';
